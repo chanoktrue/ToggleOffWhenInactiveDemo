287c514af238160b84e32c247dea1acfb05298e0
chanoktrue <chanoktrue@me.com>
2024-07-05 13:54:22 +0700

    Initial Commit

---
+++ ToggleDemo/ToggleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleDemoApp.swift
+//  ToggleDemo
+//
+//  Created by Thongchai Subsaidee on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToggleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
