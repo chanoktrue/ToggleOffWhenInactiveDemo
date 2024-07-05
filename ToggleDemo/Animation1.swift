530881c3f9b55ee99d08dbca06c817f06e99f46d
chanoktrue <chanoktrue@me.com>
2024-07-05 16:44:43 +0700

    animation1

--- ToggleDemo/Animation1.swift
+++ ToggleDemo/Animation1.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+import SwiftUI
+
+struct ContentView1: View {
+    var body: some View {
+        SiriAnimationView()
+    }
+}
+
+struct SiriAnimationView: View {
+    @State private var isAnimating = false
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 8)
+                .frame(width: 100, height: 100)
+                .scaleEffect(isAnimating ? 1 : 0.8)
+                .opacity(isAnimating ? 0 : 1)
+                .rotationEffect(.degrees(isAnimating ? 360 : 0))
+                .shadow(color: Color.red.opacity(0.5), radius: 10, x: 0, y: 0)
+                .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+            
+            Circle()
+                .stroke(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 8)
+                .frame(width: 100, height: 100)
+                .scaleEffect(isAnimating ? 0.8 : 1)
+                .opacity(isAnimating ? 1 : 0)
+                .rotationEffect(.degrees(isAnimating ? -360 : 0))
+                .shadow(color: Color.purple.opacity(0.5), radius: 10, x: 0, y: 0)
+                .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+        }
+        .onAppear {
+            isAnimating = true
+        }
+    }
+}
+
+
+#Preview(body: {
+    ContentView1()
+})
